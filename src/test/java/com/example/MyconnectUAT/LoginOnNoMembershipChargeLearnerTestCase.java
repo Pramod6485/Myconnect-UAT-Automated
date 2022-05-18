8c567b0e9bc4cb4798ca6619f1a68ccc8fe92ad6
raames <rameshdangol@olivemedia.co>
2022-05-18 16:44:23 +0545

    RegisterOnSubscripitionWithPayment automated successfully

--- src/test/java/com/example/MyconnectUAT/LoginOnNoMembershipChargeLearnerTestCase.java
+++ src/test/java/com/example/MyconnectUAT/LoginOnNoMembershipChargeLearnerTestCase.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class LoginOnNoMembershipChargeLearnerTestCase {
@@ -14,12 +18,10 @@ public class LoginOnNoMembershipChargeLearnerTestCase {
   
   @Test
   public void testLoginOnNoMembershipChargeLearnerTestCase() throws Exception {
-<<<<<<< HEAD
-	System.setProperty("webdriver.chrome.driver", "F:\\Myconnect-UAT-Automated\\driver\\chromedriver.exe");
-=======
-	System.setProperty("webdriver.chrome.driver", "P:\\Myconnect Automation With Selenium\\Myconnect-UAT-Automated\\driver\\chromedriver.exe");
->>>>>>> ce69e4693926ae235c96597b6d44f25e17a3afcd
-	driver = new ChromeDriver();
+	
+	System.setProperty("webdriver.gecko.driver", "F:\\Myconnect-UAT-Automated\\driver\\geckodriver.exe");
+	
+	driver = new FirefoxDriver();
 	driver.manage().window().maximize();
     driver.get("https://connected.uat.myconnect.olivegroup.io/learner/login");
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
--- src/test/java/com/example/MyconnectUAT/RegisterOnSubscripitionWithPaymentTestCase.java
+++ src/test/java/com/example/MyconnectUAT/RegisterOnSubscripitionWithPaymentTestCase.java
@@ -19,22 +19,26 @@ public class RegisterOnSubscripitionWithPaymentTestCase {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "P:\\Myconnect Automation With Selenium\\Myconnect-UAT-Automated\\\\driver\\chromedriver.exe");
+    //System.setProperty("webdriver.chrome.driver", "P:\\Myconnect Automation With Selenium\\Myconnect-UAT-Automated\\driver\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", "F:\\Automate\\MyConnect-Automate\\driver\\chromedriver.exe");
     driver = new ChromeDriver();
+    driver.manage().window().maximize();
     baseUrl = "https://www.google.com/";
+    driver.manage().deleteAllCookies();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) driver;
   }
 
   @Test
   public void testRegisterOnSubscripitionWithPaymentTestCase() throws Exception {
+	
     driver.get("https://connected.uat.myconnect.olivegroup.io/learner/login");
     driver.findElement(By.linkText("Register here")).click();
     driver.get("https://connected.uat.myconnect.olivegroup.io/learner/register");
     driver.findElement(By.id("email")).click();
     driver.findElement(By.id("email")).clear();
     Thread.sleep(1500);
-    driver.findElement(By.id("email")).sendKeys("payment.lea2@yopmail.com");
+    driver.findElement(By.id("email")).sendKeys("payment.lea6@yopmail.com");
     driver.findElement(By.id("password")).click();
     driver.findElement(By.id("password")).clear();
     driver.findElement(By.id("password")).sendKeys("P@ssword12");
@@ -50,10 +54,11 @@ public class RegisterOnSubscripitionWithPaymentTestCase {
     driver.findElement(By.xpath("//div[@id='app']/div/div/div/div/div/div/div/p/div[2]/input")).click();
     Thread.sleep(1500);
     driver.findElement(By.name("subscriptionOptions")).click();
-    Thread.sleep(1500);
-    driver.findElement(By.xpath("//div[@id='app']/div/div/div/div/div/div/div/button")).click();
-    Thread.sleep(2000);
-    driver.get("https://checkout.stripe.com/pay/cs_test_a1EcmnCjQl17NX1ncDyegbnSCloq7E5XLSd3gm1xSZtaFzCzY14w4Pi7C2#fidkdWxOYHwnPyd1blpxYHZxWjxsbm1DcF9dYk9jdU0yTmY0Sj1tV0JiZCcpJ2hsYXYnP34nYnBsYSc%2FJzE1M2Q2NjZgKDI8MzIoMTNhNChkPTdnKDQ8ZzYxNzdjMGc9PGFkYGMyZycpJ2hwbGEnPyc1YWAzYzw9Nig2MDU9KDE0PWcoZGEzZihgZGc3NWM8Y2ZkMzAxNWExYDcnKSd2bGEnPyc0NTcxMjQ0MShkYTBgKDE3PDEoPDVnYSgxNTIyMjFgMjBgZDw3YDY9MmMneCknZ2BxZHYnP15YKSdpZHxqcHFRfHVgJz8ndmxrYmlgWmxxYGgnKSd3YGNgd3dgd0p3bGJsayc%2FJ21xcXV2PyoqZmpra2BmcWBhK3BkcStofGZqa2tgZnEramlsc2Bid2pwdStsaid4JSUl");
+    js.executeScript("window.scrollBy(0,350)", "");
+    Thread.sleep(5000);
+    driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div/div/div/div/div/button")).click();
+    //Thread.sleep(2000);
+    //driver.get("https://checkout.stripe.com/pay/cs_test_a1EcmnCjQl17NX1ncDyegbnSCloq7E5XLSd3gm1xSZtaFzCzY14w4Pi7C2#fidkdWxOYHwnPyd1blpxYHZxWjxsbm1DcF9dYk9jdU0yTmY0Sj1tV0JiZCcpJ2hsYXYnP34nYnBsYSc%2FJzE1M2Q2NjZgKDI8MzIoMTNhNChkPTdnKDQ8ZzYxNzdjMGc9PGFkYGMyZycpJ2hwbGEnPyc1YWAzYzw9Nig2MDU9KDE0PWcoZGEzZihgZGc3NWM8Y2ZkMzAxNWExYDcnKSd2bGEnPyc0NTcxMjQ0MShkYTBgKDE3PDEoPDVnYSgxNTIyMjFgMjBgZDw3YDY9MmMneCknZ2BxZHYnP15YKSdpZHxqcHFRfHVgJz8ndmxrYmlgWmxxYGgnKSd3YGNgd3dgd0p3bGJsayc%2FJ21xcXV2PyoqZmpra2BmcWBhK3BkcStofGZqa2tgZnEramlsc2Bid2pwdStsaid4JSUl");
     Thread.sleep(5000);
     driver.findElement(By.id("email")).click();
     Thread.sleep(1500);
@@ -71,68 +76,62 @@ public class RegisterOnSubscripitionWithPaymentTestCase {
     Thread.sleep(1000);
     driver.findElement(By.id("cardExpiry")).sendKeys("06 / 56");
     Thread.sleep(1000);
+    driver.findElement(By.id("cardCvc")).sendKeys("145");
+    Thread.sleep(1000);
     driver.findElement(By.id("billingName")).clear();
     Thread.sleep(1000);
     driver.findElement(By.id("billingName")).sendKeys("Pramod Phanait");
     Thread.sleep(1000);
-    driver.findElement(By.id("cardExpiry")).click();
-    driver.findElement(By.id("cardCvc")).click();
-    driver.findElement(By.id("cardCvc")).clear();
-    Thread.sleep(1000);
-    driver.findElement(By.id("cardCvc")).sendKeys("145");
-    driver.findElement(By.id("billingName")).click();
+      driver.findElement(By.id("billingName")).click();
     Thread.sleep(1000);
     driver.findElement(By.id("billingCountry")).click();
-    Thread.sleep(1000);
-    new Select(driver.findElement(By.id("billingCountry"))).selectByVisibleText("Morocco");
-    Thread.sleep(3000);
-    driver.findElement(By.id("enableStripePass")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.id("phoneNumber")).clear();
-//    driver.findElement(By.id("phoneNumber")).sendKeys("98745216");
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div/div[2]/form/div[2]/div[2]/button/div[3]")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.id("phoneNumber")).clear();
-    driver.findElement(By.id("phoneNumber")).sendKeys("");
-    Thread.sleep(1000);
-    driver.findElement(By.id("billingCountry")).click();
-    Thread.sleep(1000);
+	driver.findElement(By.id("enableStripePass")).click();
+    Thread.sleep(5000);
+	/*
+	 * driver.findElement(By.id("phoneNumber")).sendKeys("9874521611");
+	 * js.executeScript("window.scrollBy(0,350)", ""); Thread.sleep(2000);
+	 * driver.findElement(By.xpath(
+	 * "//div[@id='root']/div/div/div[2]/div/div[2]/form/div[2]/div[2]/button/div[3]"
+	 * )).click(); Thread.sleep(1000);
+	 * driver.findElement(By.id("phoneNumber")).clear();
+	 * driver.findElement(By.id("phoneNumber")).sendKeys(""); Thread.sleep(1000);
+	 * driver.findElement(By.id("billingCountry")).click();
+	 */
     new Select(driver.findElement(By.id("billingCountry"))).selectByVisibleText("Nepal");
     Thread.sleep(1000);
-    driver.findElement(By.id("phoneNumber")).click();
-    driver.findElement(By.id("phoneNumber")).clear();
-    Thread.sleep(1000);
     driver.findElement(By.id("phoneNumber")).sendKeys("987-8451578");
-    Thread.sleep(1000);
+    js.executeScript("window.scrollBy(0,350)", "");
+    Thread.sleep(2000);
     driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div/div[2]/form/div[2]/div[2]/button/div[3]")).click();
     Thread.sleep(1000);
-    driver.get("https://connected.uat.myconnect.olivegroup.io/learner/register/in-progress?session_id=cs_test_a1vo03HW1YYCVWmI7EWdK9EFNqhQaBmFCCBWKhby3Pqn8cSPQq8Z1yiKWm");
-    Thread.sleep(6000);
-    driver.get("https://connected.uat.myconnect.olivegroup.io/learner/register");
-    Thread.sleep(3000);
-    driver.findElement(By.id("first_name")).click();
+    //driver.get("https://connected.uat.myconnect.olivegroup.io/learner/register/in-progress?session_id=cs_test_a1vo03HW1YYCVWmI7EWdK9EFNqhQaBmFCCBWKhby3Pqn8cSPQq8Z1yiKWm");
+    //Thread.sleep(6000);
+    //driver.get("https://connected.uat.myconnect.olivegroup.io/learner/register");
+	/*
+	 * Thread.sleep(3000); driver.findElement(By.id("first_name")).click();
+	 * Thread.sleep(1000); driver.findElement(By.id("first_name")).clear();
+	 */
     Thread.sleep(1000);
-    driver.findElement(By.id("first_name")).clear();
+    driver.findElement(By.id("first_name")).sendKeys("Pramod6");
     Thread.sleep(1000);
-    driver.findElement(By.id("first_name")).sendKeys("Pramod");
+	driver.findElement(By.id("last_name")).sendKeys("Test6");
     Thread.sleep(1000);
-    driver.findElement(By.id("last_name")).clear();
-    Thread.sleep(1000);
-    driver.findElement(By.id("last_name")).sendKeys("Registered");
-    Thread.sleep(1000);
+    //Click on Continue
     driver.findElement(By.xpath("//button[@type='button']")).click();
     Thread.sleep(1000);
+    //Click on Continue
     driver.findElement(By.xpath("//button[@type='button']")).click();
-    Thread.sleep(1000);
+    js.executeScript("window.scrollBy(0,350)", "");
+    Thread.sleep(2000);
+    //Select Color
     driver.findElement(By.xpath("//div[@id='app']/div/div/div/div/div/div[5]/div/button")).click();
     Thread.sleep(1000);
+    //Click Continue
     driver.findElement(By.xpath("//div[@id='app']/div/div/div/div/button")).click();
-    Thread.sleep(1000);
-    driver.get("https://connected.uat.myconnect.olivegroup.io/hub/home");
-    Thread.sleep(1000);
-    driver.findElement(By.id("imagetest")).click();
-    Thread.sleep(1000);
+    js.executeScript("window.scrollBy(0,-350)", "");
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//*[@id=\"imagetest\"]")).click();
+    Thread.sleep(5000);
     driver.findElement(By.id("logout")).click();
     driver.close();
     
